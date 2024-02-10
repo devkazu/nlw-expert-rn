59f85905cf9a3c5859fa0b2399b428faae88149f
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-02-10 01:18:16 -0300

    feat: add sectionList with items

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,27 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { FlatList, View } from "react-native";
-import { CATEGORIES } from "@/utils/data/products";
-import { useState } from "react";
+import { FlatList, Text, View, SectionList } from "react-native";
+import { CATEGORIES, MENU } from "@/utils/data/products";
+import { useState, useRef } from "react";
+import { Product } from "@/components/product";
 
 export default function Home() {
   const [category, setCategory] = useState<string>(CATEGORIES[0]);
+  const sectionListRef = useRef<SectionList>(null);
 
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory);
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === selectedCategory
+    );
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0,
+      });
+    }
   }
   return (
     <View className=" flex-1 pt-8">
@@ -27,6 +40,21 @@ export default function Home() {
         className="max-h-10 mt-5"
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
         showsHorizontalScrollIndicator={false}
+      />
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => <Product data={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        className="flex-1 p-5"
       />
     </View>
   );
