29efe41c633bf08dd9a23ac185b5739298e3489b
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-02-10 00:11:31 -0300

    chore: loading fonts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,9 @@
-import { Text, View } from 'react-native';
-
+import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="bg-slate-900 flex-1" >
-      <Text className="text-white">Hello world</Text>
+    <View className=" flex-1 pt-8">
+      <Text className="text-white text-2xl">Hello world</Text>
     </View>
   );
 }
